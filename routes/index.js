9e3688d73b64ba1c7314c56721f877cbafa464d6
didietsuryadi <didietsuryadi>
2017-03-03 08:11:33 +0700

    finish-fiture-login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/authController')
+var passport = require('passport')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('index');
+  res.send('use /login or /auth/facebook/login for authenticate and access data with /api');
 });
+
+//login-local
+
+router.post('/login',passport.authenticate('didit-login'), userController.login);
+
+router.post('/register', userController.register);
+
+//login-facebook
+router.get('/auth/facebook/login', passport.authenticate('facebook', {scope: 'email'}))
+
+router.use('/auth/facebook/callback', passport.authenticate('facebook', { successRedirect: '/auth/login/success', failureRedirect: '/auth/login/failed' }))
+
+router.get('/auth/login/failed', function (req, res) {
+  res.send('error')
+})
+
+router.get('/auth/login/success', userController.login)
 
 module.exports = router;
